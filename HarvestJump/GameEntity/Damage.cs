7935dd7b30bb8b79cc12d89e99e9ea50128f47ce
milan <milan@pancopia.net>
2016-05-09 14:44:50 +0200

    -added damage and atack class

---
+++ HarvestJump/GameEntity/Damage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarvestJump
+{
+    enum DamageType
+    {
+        physical,
+        magic,
+    }
+    class Damage
+    {
+        private int atackValue { get; set; }
+        private DamageType type { get; set; }
+
+        public Damage(int atackValue, DamageType type)
+        {
+            this.atackValue = atackValue;
+            this.type = type;
+        }
+    }
+}
---
+++ HarvestJump/GameEntity/MeleeAtack.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Audio;
+
+namespace HarvestJump
+{
+    class Atack
+    {
+        private Damage Damage { get; set; }
+        private BoundingBox DamageArea { get; set; }
+        private SoundEffect atackSound { get; set; }
+        public Atack(Vector2 position, int widht, int height, int atackValue, DamageType damageType)
+        {
+            this.Damage = new Damage(atackValue, damageType);
+
+            CreateDamageArea(position, widht, height);
+        }
+
+        private void CreateDamageArea(Vector2 position, int width, int height)
+        {
+            DamageArea = new BoundingBox(position, width, height);
+        }
+    }
+}
